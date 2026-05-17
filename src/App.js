bf579221058840fd9977250b4c4d45cf10958d6b
zamong-note <tamongnote@naver.com>
2026-05-17 19:53:30 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import { useState, useEffect } from "react";
 
-function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("")
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-    console.log("I run all the time");
-    useEffect(() => {
-        console.log("I run only onChange.");
-    }, []);
-    useEffect(() => {
-            console.log("I run when 'keyword' changes.");
-        }, [keyword]);
-    useEffect(() => {
-        console.log("I run when 'counter' changes.");
-    }, [counter]);
-    useEffect(() => {
-        console.log("I run when keyword & counter change");
-    }, [keyword, counter]);
-    return (
-        <div>
-            <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-            <h1>{counter}</h1>
-            <button onClick={onClick}>Click Me</button>
-        </div>
-    );
+function Hello() {
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
 }
 
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
 
 export default App;
